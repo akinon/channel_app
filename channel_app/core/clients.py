cf69a88b4b760a83e9e527258463ab17a64b1e30
enesrizaates <enesrates@gmail.com>
2023-10-04 11:10:20 +0300

    OMNI-2463 Add token to cache to prevent additional requests for authentication

--- channel_app/core/clients.py
+++ channel_app/core/clients.py
@@ -1,5 +1,6 @@
 from redis import Redis
-
+from omnisdk.omnitron.client import OmnitronApiClient as BaseOmnitronApiClient
+v
 
 class RedisClient(Redis):
     def __init__(self):
@@ -10,3 +11,22 @@ class RedisClient(Redis):
         super(RedisClient, self).__init__(host=self.broker_host,
                                           port=self.broker_port,
                                           db=self.broker_database_index)
+
+
+class OmnitronApiClient(BaseOmnitronApiClient):
+    """
+    OmnitronApiClient class for Omnitron API requests.
+    """
+    client_route = "api/v1/"
+    redis_prefix = "omnitron_auth_token_prefix"
+
+    def __init__(self, base_url, username, password):
+        self.redis_client = RedisClient()
+        super().__init__(base_url, username, password)
+
+    @property
+    def token(self):
+        return self.redis_client.get(self.redis_prefix)
+
+    def set_token(self, token):
+        self.redis_client.set(self.redis_prefix, token)
