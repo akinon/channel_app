edbf20c3ee5e71897262589c1a1589303788290b
ziyacivan <yusufziya.civan@akinon.com>
2023-12-13 16:17:50 +0300

    OMNI-2710: Update the package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="channel_app",
-    version="0.0.135",
+    version="0.0.136",
     packages=find_packages(),
     url="https://github.com/akinon/channel_app",
     description="Channel app for Sales Channels",
